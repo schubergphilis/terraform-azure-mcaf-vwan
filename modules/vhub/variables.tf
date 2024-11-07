60fe8f7ca8c5fc00d25383b9780b78d6eb29978d
Gillian Stravers <gillianstravers@icloud.com>
2024-11-07 09:33:58 +0100

    removed a variable

--- modules/vhub/variables.tf
+++ modules/vhub/variables.tf
@@ -3,7 +3,6 @@ variable "virtual_hubs" {
     name                     = string
     location                 = string
     address_prefix           = string
-    extended_vnet            = bool
     firewall_sku_tier        = string
     firewall_public_ip_count = number
   })
