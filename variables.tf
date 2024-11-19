c4237b836fe1ec8f9e84b477dad2b0c9581fff74
Gillian Stravers <gillianstravers@icloud.com>
2024-11-19 14:28:05 +0100

    changed hubs to map object

--- variables.tf
+++ variables.tf
@@ -13,18 +13,17 @@ variable "virtual_wan" {
 }
 
 variable "virtual_hubs" {
-  type = object({
+  type = map(object({
     virtual_hub_name          = string
-    location                 = string
-    address_prefix           = string
-    routing_intent_name      = string
-    firewall_name            = string
-    firewall_policy_name     = string
-    firewall_sku_tier        = string
-    firewall_public_ip_count = number
+    location                  = string
+    address_prefix            = string
+    routing_intent_name       = string
+    firewall_name             = string
+    firewall_policy_name      = string
+    firewall_sku_tier         = string
+    firewall_public_ip_count  = number
     firewall_threat_intelligence_mode = string
-    firewall_proxy_enabled   = bool
-    firewall_dns_servers     = list(string)
-
-  })
+    firewall_proxy_enabled    = bool
+    firewall_dns_servers      = list(string)
+  }))
 }
