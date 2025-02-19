a2d357ec71e0b5fdba1fffb33e97241e7af6cdb7
fblankendaal <fblankendaal@schubergphilis.com>
2025-02-19 11:02:10 +0100

    change name of the proxy, it is a dns proxy to be more precise

--- variables.tf
+++ variables.tf
@@ -1,35 +1,3 @@
-# variable "virtual_hubs"
-# This variable defines the configuration for virtual hubs.
-# - virtual_hub_name: The name of the virtual hub (string).
-# - location: The location/region of the virtual hub (string).
-# - address_prefix: The address prefix for the virtual hub (string).
-# - routing_intent_name: The name of the routing intent (string).
-# - firewall_name: The name of the firewall (string).
-# - firewall_policy_name: The name of the firewall policy (string).
-# - firewall_sku_tier: The SKU tier of the firewall (string).
-# - firewall_public_ip_count: The number of public IPs for the firewall (number).
-# - firewall_threat_intelligence_mode: The threat intelligence mode for the firewall (string).
-# - firewall_proxy_enabled: Whether the firewall proxy is enabled (bool).
-# - firewall_dns_servers: A list of DNS servers for the firewall (list of strings).
-
-# variable "tags"
-# This variable defines a map of tags to be applied to resources.
-# - type: A map of strings.
-# - default: An empty map by default.
-
-
-# variable "resource_group_name"
-# This variable defines the name of the resource group.
-# - type: string.
-
-# variable "hub_bgp_peers"
-# This variable defines the configuration for BGP peers.
-# - virtual_hub_id: The ID of the virtual hub (string).
-# - name: The name of the BGP peer (string).
-# - peer_asn: The ASN of the BGP peer (number).
-# - peer_ip: The IP address of the BGP peer (string).
-# - vnet_connection_id: The ID of the VNET Hub connection (string).
-
 variable "resource_group_name" {
   type        = string
   description = "The name of the resource group where the VWAN will be created"
@@ -70,7 +38,7 @@ variable "virtual_hubs" {
     firewall_sku_tier                 = string
     firewall_public_ip_count          = number
     firewall_threat_intelligence_mode = string
-    firewall_proxy_enabled            = optional(bool, true)
+    firewall_dns_proxy_enabled        = optional(bool, true)
     firewall_dns_servers              = list(string)
   }))
   default     = {}
@@ -85,8 +53,8 @@ This variable defines the configuration for virtual hubs.
 - firewall_sku_tier: The SKU tier of the firewall (string).
 - firewall_public_ip_count: The number of public IPs for the firewall (number).
 - firewall_threat_intelligence_mode: The threat intelligence mode for the firewall (string).
-- firewall_proxy_enabled: Whether the firewall proxy is enabled (bool).
-- firewall_dns_servers: A list of DNS servers for the firewall (list of strings).
+- firewall_dns_proxy_enabled: Whether the firewall dns proxy is enabled (bool).
+- firewall_dns_servers: A list of DNS servers for the firewall (list of string).
 DESCRIPTION
 }
 
