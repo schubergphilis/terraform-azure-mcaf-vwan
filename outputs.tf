6812fa82d778046b7dc16cd7cf16c7cb17f0d814
Ferry Blankendaal <fblankendaal@schubergphilis.com>
2025-03-11 13:21:52 +0100

    add some outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,11 @@ output "resource_group_id" {
   description = "ID of the Resource Group created by the module"
   value       = azurerm_resource_group.this.id
 }
+
+output "virtual_hubs" {
+  value = azurerm_virtual_wan.this.virtual_hubs
+}
+
+output "vpn_sites" {
+  value = azurerm_virtual_wan.this.vpn_sites
+}
