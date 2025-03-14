0839090b1c9c36a96c7f23c600e824d9c78c8a8e
Nelesh Goel <ngoel@schubergphilis.com>
2025-03-14 11:17:19 +0100

    firewall_public_ip_count

--- modules/vhub/main.tf
+++ modules/vhub/main.tf
@@ -33,7 +33,7 @@ resource "azurerm_firewall" "this" {
 
   virtual_hub {
     virtual_hub_id  = azurerm_virtual_hub.this.id
-    public_ip_count = var.virtual_hubs.use_byoip ? 0 : var.virtual_hubs.firewall_public_ip_count
+    public_ip_count = var.virtual_hubs.use_byoip ? max(var.virtual_hubs.firewall_public_ip_count, 1) : var.virtual_hubs.firewall_public_ip_count
   }
 
   dynamic "ip_configuration" {
